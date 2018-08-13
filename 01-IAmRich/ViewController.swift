287342342a26ca79fd212a8a410213686f6b8f0d
fabian andres benavides labiano <fabianiniprz@gmail.com>
2018-08-13 02:26:55 -0500

    Initial Commit

---
+++ 01-IAmRich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  01-IAmRich
+//
+//  Created by Fabian andres Benavides labiano on 13/08/18.
+//  Copyright © 2018 Fabian andres Benavides labiano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
